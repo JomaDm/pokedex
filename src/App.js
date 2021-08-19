da767d3d0a25af8e2b37fbba48c4a427fffb0c25
Joma Dm <Joma_Dm@outlook.com>
2021-08-18 19:02:24 -0500

    Probando la api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [links, setLinks] = useState({});
+
+	useEffect(() => {
+		axios
+			.get('https://pokeapi.co/api/v2/')
+			.then((response) => {
+				console.log(response.data);
+				setLinks(response.data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	});
+
+	return (
+		<div className="App">
+			{Object.keys(links).map((keyLink, index) => {
+				return (
+					<p key={index}>
+						{keyLink}: {links[keyLink]}
+					</p>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
