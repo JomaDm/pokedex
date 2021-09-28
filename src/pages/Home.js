2693bbda486f03d60aa5a7ae88eadc08bfb86847
Joma Dm <Joma_Dm@outlook.com>
2021-09-27 23:10:32 -0500

    Agregado diseño sencillo en Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import './Home.css';
+
 const Home = () => {
 	return (
-		<div>
-			<h1>Home Page</h1>
+		<div className='home-container'>
+			<img
+				src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png'
+				alt='Pokeball'
+			/>
+			<div>
+				<h2>Pokedex Project</h2>
+				<p>Este proyecto lo realizo un fan de pokemon para fans de pokemon.</p>
+				<h3>¿Que ofrece esta pagina?</h3>
+				<p>En esta sencilla pagina puedes buscar a tus pokemon's favoritos de cualquier generacion, ver sus estadisticas  y además podras ver sus habilidades y movimientos</p>
+			</div>
 		</div>
 	);
 };
