cfbe18086cd352e1c008654f577de765df1661ba
Joma Dm <Joma_Dm@outlook.com>
2021-09-27 19:04:09 -0500

    Componente mostrados agregado

---
+++ src/components/Mostrados.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Mostrados.css';
+
+const Mostrar = ({ setChunkSize }) => {
+	return (
+		<div className='cantidad-mostrada'>
+			<label htmlFor='chunk-size'>Mostrados: </label>
+			<select name='chunk-size' onChange={(evnt) => setChunkSize(parseInt(evnt.target.value))} id='chunk-size'>
+				<option value='20'>20</option>
+				<option value='50'>50</option>
+				<option value='100'>100</option>
+				<option value='200'>200</option>
+				<option value='500'>500</option>
+			</select>
+		</div>
+	);
+};
+
+export default Mostrar;
